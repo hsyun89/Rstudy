5be22b3f2c2b66bf816a393c887219d6e81007c8
hsyun89 <hsyun89@naver.com>
2019-09-06 17:14:12 +0900

    .

---
+++ daumnews_schedule.R
@@ -0,0 +1,20 @@
+# 단일 페이지(rvest 패키지 사용)
+#install.packages("rvest"); 
+library(rvest)
+library(dplyr)
+url<- "http://media.daum.net/ranking/popular/"
+text <- read_html(url,  encoding="UTF-8")
+# newtitle
+nodes <- html_nodes(text, "ul.list_news2 a.link_txt")
+newstitle <- html_text(nodes)
+newstitle <- gsub("[[:punct:][:cntrl:]]", "", newstitle)
+newstitle
+# newspaper
+nodes <- html_nodes(text, "ul.list_news2 span.info_news")
+newspapername <- html_text(nodes, trim=TRUE)
+newspapername <- gsub("[[:punct:][:cntrl:]]", "", newspapername)
+newspapername
+page <- cbind(newstitle, newspapername) %>% head(5)
+
+
+##write.csv(page, "daumnews.csv")
